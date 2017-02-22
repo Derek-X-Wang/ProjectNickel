a7e58f920667e35a7e8da40ec5abc2d4c4875bae
Derek Wang <derekxinzhewang@gmail.com>
2017-02-22 03:05:17 -0800

    New -> Add rest framework & website template

--- website/views.py
+++ website/views.py
@@ -7,6 +7,6 @@ import logging
 logger = logging.getLogger(__name__)
 # Create your views here.
 def index(request):
-    data = quandl.get("WIKI/FB")
-    logger.error("data")
+    # data = quandl.get("WIKI/FB")
+    # logger.error("data")
     return render(request, 'website/index.html')
